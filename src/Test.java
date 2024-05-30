849a602cd11a8f4769b3c42559f595b72b4761f0
JEESW <jeesw1221@gmail.com>
2024-05-30 18:09:47 +0900

    init

--- src/Test.java
+++ src/Test.java
@@ -8,9 +8,8 @@ public class Test {
      * @hidden
      **/
     public static void main(String[] args) {
-        int a = 10;
-        System.out.println(a);
-        System.out.println(a);
-        System.out.println(a);
+        System.out.println("Hello 오오 오징어");
+        System.out.println("Hello 오오 오징어");
+        System.out.println("Hello 오오 오징어");
     }
 }
