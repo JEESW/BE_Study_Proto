b8849d4366ab1563650bf33026dda65bec082c3a
JEESW <jeesw1221@gmail.com>
2024-06-14 00:52:20 +0900

    init

--- src/DailyQuiz_240611.java
+++
@@ -1,9 +0,0 @@
-public class DailyQuiz_240611 {
-  public static void main(String[] args) {
-    DailyQuizMySelf i = new DailyQuizMySelf("sleepy");
-    DailyQuizFriend fnd = new DailyQuizFriend("Al phabet", 20);
-
-    i.infoI();
-    i.bothering(fnd);
-  }
-}
