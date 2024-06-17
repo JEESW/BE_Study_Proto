f5120cf169d766971e85f610b54aa262f28cac50
JEESW <jeesw1221@gmail.com>
2024-06-17 18:28:46 +0900

    init

--- src/Test.java
+++ src/Test.java
@@ -1,35 +1,47 @@
 public class Test {
   public static void main(String[] args) {
-    ShoppingMall shoppingMall = new PremiumShoppingMall(2);
+    //    ZooKeeper zooKeeper = new ZooKeeper();
+    //    Tiger tiger = new Tiger();
+    //    zooKeeper.feed(tiger);
+    //
+    //    Lion lion = new Lion();
+    //    zooKeeper.feed(lion);
+    //
+    //    Rabbit rabbit = new Rabbit();
+    //    zooKeeper.feed2(rabbit);
+    //
+    //    Deer deer = new Deer();
+    //    zooKeeper.feed2(deer);
 
-    Clothing c1 = new Clothing("한복", 300000, 5, "XL");
-    Clothing c2 = new Clothing("무난한 셔츠", 35000, 31, "M");
+    //    Phone applePhone = new ApplePhone();
+    //    Phone samsungPhone = new SamsungPhone();
+    //
+    //    System.out.println(applePhone.call());
+    //    System.out.println(samsungPhone.call());
+    //
+    //    System.out.println(applePhone.answerThePhone());
+    //    System.out.println(samsungPhone.answerThePhone());
 
-    Electronics e1 = new Electronics("모니터", 420000, 11, "Apple");
-    Electronics e2 = new Electronics("냉장고", 860000, 2, "Samsung");
+    //    InterfaceC impleC = new ImpleC();
+    //
+    //    InterfaceA interfaceA = impleC;
+    //    interfaceA.methodA();
+    //
+    //    InterfaceB interfaceB = impleC;
+    //    interfaceB.methodB();
+    //
+    //    impleC.methodA();
+    //    impleC.methodB();
+    //    impleC.methodC();
 
-    Food f1 = new Food("햄", 3000, 30, 7);
-    Food f2 = new Food("치즈", 4000, 45, 10);
+    //    Car kiaCar = new KiaCar();
+    //
+    //    System.out.println("타이어는 " + kiaCar.getTire() + "입니다.");
+    //    System.out.println("엔진은 " + kiaCar.showEngine() + "입니다.");
+    //    System.out.println("내비게이터는 " + kiaCar.amountNavi() + "원입니다.");
 
-    shoppingMall.addProduct(c1);
-    shoppingMall.addProduct(c2);
-    shoppingMall.displayProducts();
-    shoppingMall.addProduct(e1);
-    shoppingMall.displayProducts(1 );
-    shoppingMall.addProduct(e2);
-    shoppingMall.displayProducts();
-    shoppingMall.removeProduct(c1);
-    shoppingMall.removeProduct(c2);
-    shoppingMall.removeProduct(e1);
-    shoppingMall.displayProducts();
-    shoppingMall.addProduct(f1);
-    shoppingMall.addProduct(f2);
-    shoppingMall.displayProducts(2, 3);
-
-    System.out.println();
-    System.out.println("checkOrderAvailability c1?");
-    System.out.println(shoppingMall.checkOrderAvailability(c1));
-    System.out.println("checkOrderAvailability c2?");
-    System.out.println(shoppingMall.checkOrderAvailability(c2));
+    //    ProfileService profileService = new ProfileService();
+    //    profileService.saveProfile(new ProfileMemoryRepository());
+    //    profileService.saveProfile(new ProfileDBRepository());
   }
 }
